bd08b643812c4c5f23372a5961a417c3f6535b9c
zinzin <zinzin.vung@gmail.com>
2022-08-29 10:37:09 +0700

    update

--- src/pages/home/ProductDetail/index.js
+++ src/pages/home/ProductDetail/index.js
@@ -1,1 +1,11 @@
 import '../../../assets/scss/ProductDetail/ProductDetail.scss'
+
+const ProductDetail = () =>{
+    return (
+        <div className = "product-detail">
+
+        </div>
+    )
+}
+
+export default ProductDetail
